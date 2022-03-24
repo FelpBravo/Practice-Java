9ea850f28f7b7f577608b188e91171e4de6280e0
Felipe <felipe.bravo.espinosa@gmail.com>
2022-03-23 23:38:36 -0300

    Final del proyecto ventas

--- Sistema_de_Ventas/src/mx/com/gm/ventas/Orden.java
+++ Sistema_de_Ventas/src/mx/com/gm/ventas/Orden.java
@@ -1,5 +1,42 @@
 package mx.com.gm.ventas;
 
 public class Orden {
+    private int idOrden;
+    private Producto productos[];
+    private static int contadorOrdenes;
+    private int contadorProductos;
+    private static final int MAX_PRODUCTOS = 10;
+
+    public Orden(){
+        this.idOrden = ++Orden.contadorOrdenes;
+        this.productos = new Producto[Orden.MAX_PRODUCTOS];
+    }
+
+    public void agregarProducto(Producto producto){
+        if(this.contadorProductos < Orden.MAX_PRODUCTOS){
+            this.productos[this.contadorProductos++] = producto;
+        }else{
+            System.out.println("Se ha superado el maximo de productos " + Orden.MAX_PRODUCTOS);
+        }
+    }
+
+    public double calcularTotal(){
+        double total = 0;
+        for(int i = 0; i < this.contadorProductos; i++){
+            Producto producto = this.productos[i];
+            total += producto.getPrecio();
+        }
+        return total;
+    }
+
+    public void mostrarOrden(){
+        System.out.println("###### Orden °N" + this.idOrden + "############");
+        double totalOrden = calcularTotal();
+        System.out.println("Total de la Orden $" + totalOrden);
+        for(int i = 0; i < this.contadorProductos; i++){
+            System.out.println("Producto °N" + i + ":" + this.productos[i]);
+        }
+        System.out.println("###################################");
+    }
 
 }
--- Sistema_de_Ventas/src/mx/com/gm/ventas/test/VentasTest.java
+++ Sistema_de_Ventas/src/mx/com/gm/ventas/test/VentasTest.java
@@ -1,7 +1,16 @@
 package mx.com.gm.ventas.test;
 
+import mx.com.gm.ventas.Orden;
+import mx.com.gm.ventas.Producto;
+
 public class VentasTest {
     public static void main(String[] args) {
-
+        Orden orden = new Orden();
+        orden.mostrarOrden();
+        Producto producto1 = new Producto("Teclado", 15.000);
+        Producto producto2 = new Producto("Mouse", 3.000);
+        orden.agregarProducto(producto1);
+        orden.agregarProducto(producto2);
+        orden.mostrarOrden();
     }
 }
